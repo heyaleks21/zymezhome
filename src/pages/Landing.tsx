2639e3b2e0881862c83c37003f3125bf7eb5e82a
heyaleks21 <oceboosting@gmail.com>
2023-07-09 22:15:30 +0930

    21

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 const Landing = () => {
   return (
     <>
-      <div className="min-w-screen flex min-h-screen scale-75 items-center justify-center gap-12 sm:scale-100">
+      <div className="min-w-screen flex min-h-screen scale-75 items-center justify-center sm:scale-100">
         <div className="flex w-96 flex-col items-center justify-start gap-4">
           <h1 className="flex-col items-center justify-center text-center text-5xl tracking-tight text-white lg:text-[5rem] lg:font-extrabold">
             Welcome To
             <span className="text-[var(--color4)]"> Zymez</span>
           </h1>
-          <div className="pt-2 text-center text-2xl text-[var(--text-color)]">
+          <div className="pt-1 text-center text-2xl text-[var(--text-color)]">
             Identify. Solve. Excecute.
           </div>
           <div className="text-center text-[var(--text-color)]">

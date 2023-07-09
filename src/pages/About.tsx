b2239f9c075578bebb2c1c269aaa481a877d75bc
heyaleks21 <oceboosting@gmail.com>
2023-07-10 07:15:16 +0930

    21

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 const About = () => {
   return (
     <>
-      <div className="min-w-screen flex min-h-screen scale-75 flex-wrap items-center justify-center gap-12 lg:scale-100 xl:gap-56">
-        <div className="flex w-96 flex-col items-center justify-start gap-4">
+      <div className="min-w-screen flex min-h-screen scale-75 flex-wrap items-center justify-center gap-12 xl:scale-100 xl:gap-56">
+        <div className="flex w-96 flex-col items-center justify-center gap-4">
           <h1 className="flex items-center justify-center text-center text-5xl tracking-tight text-white lg:text-[5rem] lg:font-extrabold">
             About Us
           </h1>

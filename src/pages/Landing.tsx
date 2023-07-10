054526d8d7e764b0c792c2d5475f318b655aa09c
heyaleks21 <oceboosting@gmail.com>
2023-07-10 17:01:50 +0930

    21

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,14 +5,14 @@ const Landing = () => {
     <>
       <div className="min-w-screen flex min-h-screen scale-75 items-center justify-center sm:scale-100">
         <div className="flex w-96 flex-col items-center justify-start gap-4">
-          <h1 className="flex-col items-center justify-center text-center text-5xl tracking-tight text-white lg:text-[5rem] lg:font-extrabold">
+          <h1 className="slidedown flex-col items-center justify-center text-center text-5xl tracking-tight text-white lg:text-[5rem] lg:font-extrabold">
             Welcome To
             <span className="text-[var(--color4)]"> Zymez</span>
           </h1>
-          <div className="pt-1 text-center text-2xl text-[var(--text-color)]">
+          <div className="slideside pt-1 text-center text-2xl text-[var(--text-color)]">
             Identify. Solve. Excecute.
           </div>
-          <div className="text-center text-[var(--text-color)]">
+          <div className="slideside- text-center text-[var(--text-color)]">
             Supercharge your business with outstanding software development and
             support services from Australia&apos;s trusted leader
           </div>

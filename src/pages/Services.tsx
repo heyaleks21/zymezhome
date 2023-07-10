e6494eb3fb7806ec01efe4bb7915c8995b736ccd
heyaleks21 <oceboosting@gmail.com>
2023-07-10 17:44:45 +0930

    21

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -25,7 +25,7 @@ const Services = () => {
           </p>
           <div className="flex flex-row items-center justify-start gap-5 sm:flex-col">
             <div className="flex flex-col flex-wrap items-center justify-center gap-4 sm:flex-row">
-              <div className="hover:text-none flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/90 shadow-lg transition-all duration-500 [transform-style:preserve-3d] hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
+              <div className="hover:text-none flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/70 shadow-lg transition-all duration-500 [transform-style:preserve-3d] hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
                 <p className="font-semibold">Web design</p>
                 <Image alt="" className="" src={webImg}></Image>
                 <div className="absolute inset-0 flex h-full w-full items-center items-center justify-center justify-center rounded bg-[var(--color5)] p-4 text-center text-[var(--color3)] shadow-lg [backface-visibility:hidden] [transform:rotateX(180deg)]">
@@ -35,7 +35,7 @@ const Services = () => {
                   </p>
                 </div>
               </div>
-              <div className="flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/80 shadow-lg transition-all duration-500 [transform-style:preserve-3d] hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
+              <div className="flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/70 shadow-lg transition-all duration-500 [transform-style:preserve-3d] hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
                 <p className="font-semibold">App development</p>
                 <Image alt="" className="" src={appImg}></Image>
                 <div className="absolute inset-0 flex h-full w-full items-center items-center justify-center justify-center rounded bg-[var(--color5)] p-4 text-center text-[var(--color3)] shadow-lg [backface-visibility:hidden] [transform:rotateX(180deg)]">
@@ -56,7 +56,7 @@ const Services = () => {
                   </p>
                 </div>
               </div>
-              <div className="flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/60 shadow-lg transition-all duration-500  [transform-style:preserve-3d]  hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
+              <div className="flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/70 shadow-lg transition-all duration-500  [transform-style:preserve-3d]  hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
                 <p className="font-semibold">Data solutions</p>
                 <Image alt="" className="" src={dataImg}></Image>
                 <div className="absolute inset-0 flex h-full w-full items-center items-center justify-center justify-center rounded bg-[var(--color5)] p-4 text-center text-[var(--color3)] shadow-lg [backface-visibility:hidden] [transform:rotateX(180deg)]">
@@ -68,7 +68,7 @@ const Services = () => {
               </div>
             </div>
             <div className="flex flex-col flex-wrap items-center justify-center gap-4 sm:flex-row">
-              <div className="flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/50 shadow-lg transition-all duration-500  [transform-style:preserve-3d]  hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
+              <div className="flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/70 shadow-lg transition-all duration-500  [transform-style:preserve-3d]  hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
                 <p className="font-semibold">Marketing</p>
                 <Image alt="" className="" src={marketImg}></Image>
                 <div className="absolute inset-0 flex h-full w-full items-center items-center justify-center justify-center rounded bg-[var(--color5)] p-4 text-center text-[var(--color3)] shadow-lg [backface-visibility:hidden] [transform:rotateX(180deg)]">
@@ -78,7 +78,7 @@ const Services = () => {
                   </p>
                 </div>
               </div>
-              <div className="flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/40 shadow-lg transition-all duration-500  [transform-style:preserve-3d]  hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
+              <div className="flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/70 shadow-lg transition-all duration-500  [transform-style:preserve-3d]  hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
                 <p className="font-semibold">Analytics</p>
                 <Image alt="" className="" src={analImg}></Image>
                 <div className="absolute inset-0 flex h-full w-full items-center items-center justify-center justify-center rounded bg-[var(--color5)] p-4 text-center text-[var(--color3)] shadow-lg [backface-visibility:hidden] [transform:rotateX(180deg)]">
@@ -88,7 +88,7 @@ const Services = () => {
                   </p>
                 </div>
               </div>
-              <div className="flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/30 shadow-lg transition-all duration-500  [transform-style:preserve-3d]  hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
+              <div className="flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/70 shadow-lg transition-all duration-500  [transform-style:preserve-3d]  hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
                 <p className="font-semibold">AI integration</p>
                 <Image alt="" className="" src={aiImg}></Image>
                 <div className="absolute inset-0 flex h-full w-full items-center items-center justify-center justify-center rounded bg-[var(--color5)] p-4 text-center text-[var(--color3)] shadow-lg [backface-visibility:hidden] [transform:rotateX(180deg)]">
@@ -98,7 +98,7 @@ const Services = () => {
                   </p>
                 </div>
               </div>
-              <div className="flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/20 shadow-lg transition-all duration-500  [transform-style:preserve-3d]  hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
+              <div className="flex h-36 w-36 flex-col items-center justify-center gap-3 rounded bg-white/70 shadow-lg transition-all duration-500  [transform-style:preserve-3d]  hover:[transform:rotateX(180deg)] lg:h-44 lg:w-44 xl:h-52 xl:w-52">
                 <p className="font-semibold">Support services</p>
                 <Image alt="" className="" src={monitorImg}></Image>
                 <div className="absolute inset-0 flex h-full w-full items-center items-center justify-center justify-center rounded bg-[var(--color5)] p-4 text-center text-[var(--color3)] shadow-lg [backface-visibility:hidden] [transform:rotateX(180deg)]">

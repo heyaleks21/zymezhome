960d84e0de8800309ab2903b141209299cddecc7
heyaleks21 <oceboosting@gmail.com>
2023-07-10 17:38:53 +0930

    21

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,8 +4,8 @@ const Landing = () => {
   return (
     <>
       <div className="min-w-screen flex min-h-screen scale-75 items-center justify-center overflow-hidden sm:scale-100">
-        <div className="flex w-96 flex-col items-center justify-start gap-4">
-          <h1 className="slidedown flex-col items-center justify-center text-center text-5xl tracking-tight text-white lg:text-[5rem] lg:font-extrabold">
+        <div className="slidedown flex w-96 flex-col items-center justify-start gap-4">
+          <h1 className="flex-col items-center justify-center text-center text-5xl tracking-tight text-white lg:text-[5rem] lg:font-extrabold">
             Welcome To
             <span className="text-[var(--color4)]"> Zymez</span>
           </h1>
